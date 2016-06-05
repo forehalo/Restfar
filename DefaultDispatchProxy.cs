b3922c6843708b04dcdef8a292166b45c614a68d
forehalo <forehalo@gmail.com>
2016-06-05 16:09:24 +0800

    add custom dispatch proxy

---
+++ DefaultDispatchProxy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+namespace Restfar
+{
+    public class DefaultDispatchProxy : DispatchProxy
+    {
+        protected override object Invoke(MethodInfo targetMethod, object[] args)
+        {
+            if (targetMethod.DeclaringType == typeof(object))
+                return targetMethod.Invoke(this, args);
+
+
+            var serviceMethod = LoadServiceMethod(targetMethod);
+            dynamic obj;
+            if (targetMethod.ReturnType.IsConstructedGenericType)
+            {
+                var returnType = targetMethod.ReturnType.GetGenericArguments()[0];
+                obj = Activator.CreateInstance(returnType);
+            }
+            else
+            {
+                obj = new object();
+            }
+            return serviceMethod.Call(obj, args);
+        }
+
+        private static ServiceMethod LoadServiceMethod(MethodInfo targetMethod)
+        {
+            ServiceMethod result = null;
+            //TODO Cache
+            if (result == null)
+            {
+                result = new ServiceMethod(targetMethod);
+            }
+            return result;
+        }
+    }
+}
---
+++ RestfarClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reflection;
+namespace Restfar
+{
+    public class RestfarClient
+    {
+        public static T Create<T>()
+        {
+            return DispatchProxy.Create<T, DefaultDispatchProxy>();
+        }
+    }
+}
