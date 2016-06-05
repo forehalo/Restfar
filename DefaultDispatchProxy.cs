437f916444fe87cc20f9aed531d0075159898691
forehalo <forehalo@gmail.com>
2016-06-05 16:25:16 +0800

    move loadServiceMethod function to RestfarClient class

--- DefaultDispatchProxy.cs
+++ DefaultDispatchProxy.cs
@@ -2,15 +2,26 @@
 using System.Reflection;
 namespace Restfar
 {
+    /// <summary>
+    /// Dispatch proxy used to dispatch call of unimplemented interface method.
+    /// </summary>
     public class DefaultDispatchProxy : DispatchProxy
     {
+        /// <summary>
+        /// Invoke the called method
+        /// </summary>
+        /// <param name="targetMethod"></param>
+        /// <param name="args"></param>
+        /// <returns>the required result</returns>
         protected override object Invoke(MethodInfo targetMethod, object[] args)
         {
             if (targetMethod.DeclaringType == typeof(object))
                 return targetMethod.Invoke(this, args);
 
-
-            var serviceMethod = LoadServiceMethod(targetMethod);
+            var serviceMethod = RestfarClient.LoadServiceMethod(targetMethod);
+            // Dynamic `Call` function return type.
+            // If return type of targetMethod is not generic(Task only), pass object as generic type
+            // If generic return type(Task<T>), pass the instance of T as generic type.
             dynamic obj;
             if (targetMethod.ReturnType.IsConstructedGenericType)
             {
@@ -22,17 +33,6 @@ namespace Restfar
                 obj = new object();
             }
             return serviceMethod.Call(obj, args);
-        }
-
-        private static ServiceMethod LoadServiceMethod(MethodInfo targetMethod)
-        {
-            ServiceMethod result = null;
-            //TODO Cache
-            if (result == null)
-            {
-                result = new ServiceMethod(targetMethod);
-            }
-            return result;
         }
     }
 }
--- RestfarClient.cs
+++ RestfarClient.cs
@@ -2,11 +2,25 @@
 using System.Reflection;
 namespace Restfar
 {
+    /// <summary>
+    /// Create service interface caller use reflection and config the client.
+    /// </summary>
     public class RestfarClient
     {
         public static T Create<T>()
         {
             return DispatchProxy.Create<T, DefaultDispatchProxy>();
+        }
+
+        public static ServiceMethod LoadServiceMethod(MethodInfo targetMethod)
+        {
+            ServiceMethod result = null;
+            //TODO Cache
+            if (result == null)
+            {
+                result = new ServiceMethod(targetMethod);
+            }
+            return result;
         }
     }
 }
