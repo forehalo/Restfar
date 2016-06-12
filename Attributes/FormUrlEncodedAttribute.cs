af1cd1bb09a079fee31395a4f5deec1816c5032b
forehalo <forehalo@gmail.com>
2016-06-12 21:49:37 +0800

    add query attribute

--- Attributes/FormUrlEncodedAttribute.cs
+++ Attributes/FormUrlEncodedAttribute.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Restfar.Attributes
 {
     [AttributeUsage(AttributeTargets.Method)]
-    class FormUrlEncodedAttribute : Attribute
+    public class FormUrlEncodedAttribute : Attribute
     {
 
     }
---
+++ Attributes/QueryAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Restfar.Attributes
+{
+    [AttributeUsage(AttributeTargets.Parameter)]
+    public class QueryAttribute : Attribute
+    {
+        public QueryAttribute(string value)
+        {
+            Value = value;
+        }
+
+        public string Value { get; set; }
+    }
+}
