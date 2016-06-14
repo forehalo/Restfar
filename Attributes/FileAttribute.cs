bf7a1cf57088e5ebda79154321d7a0f9828b4d76
forehalo <forehalo@gmail.com>
2016-06-14 16:57:15 +0800

    file and part Attribute

---
+++ Attributes/FileAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Restfar.Attributes
+{
+    [AttributeUsage(AttributeTargets.Parameter)]
+    class FileAttribute : Attribute
+    {
+        public FileAttribute(string filePath)
+        {
+            FilePath = filePath;
+        }
+
+        public string FilePath { get; set; }
+    }
+}
