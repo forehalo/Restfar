a5ed4a9e40ea24e71c58869265b2872f6e77b3ac
forehalo <forehalo@gmail.com>
2016-06-15 19:56:38 +0800

    new head method attribute

---
+++ Attributes/HeadAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Restfar.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    class HeadAttribute : Attribute
+    {
+        public HeadAttribute(string value)
+        {
+            Value = value;
+        }
+        public string Value { get; set; }
+    }
+}
